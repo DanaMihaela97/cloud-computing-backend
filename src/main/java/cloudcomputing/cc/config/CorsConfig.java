71b16f1277c9b8bb8c598d407b52dfbab06dd063
DanaMihaela97 <danamaritca@gmail.com>
2024-05-10 15:53:43 +0300

    cors

--- src/main/java/cloudcomputing/cc/config/CorsConfig.java
+++ src/main/java/cloudcomputing/cc/config/CorsConfig.java
@@ -1,6 +1,10 @@
 package cloudcomputing.cc.config;
 
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
@@ -11,5 +15,15 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://34.203.75.47:4200")
                 .allowedMethods("GET", "POST")
                 .allowedHeaders("*");
+    }
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://34.203.75.47:4200");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter();
     }
 }
