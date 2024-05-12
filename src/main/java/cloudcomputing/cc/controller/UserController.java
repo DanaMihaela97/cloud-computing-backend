a99ad0e7e6355c37ebee94770a18e4a33b3e5bc9
DanaMihaela97 <danamaritca@gmail.com>
2024-05-12 14:18:16 +0300

    save pdf added

--- src/main/java/cloudcomputing/cc/controller/UserController.java
+++ src/main/java/cloudcomputing/cc/controller/UserController.java
@@ -5,11 +5,11 @@ import cloudcomputing.cc.entity.User;
 import cloudcomputing.cc.service.UserService;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
 import java.io.IOException;
 
 @RestController
@@ -28,31 +28,21 @@ public class UserController {
     @Autowired
     private AWSClientConfig awsClientConfig;
 
-    @PostMapping("/apply")
-    public String createUser(@RequestParam("file") MultipartFile file,
-                             @RequestParam("firstName") String firstName,
-                             @RequestParam("lastName") String lastName,
-                             @RequestParam("email") String email,
-                             @RequestParam("phone") String phone, User user) {
+    @PostMapping(path="/apply", consumes={MediaType.MULTIPART_FORM_DATA_VALUE})
+    public String createUser(@RequestPart User user, @RequestPart MultipartFile cv) {
+
         try {
-            File localFile = convertMultipartFileToFile(file);
-
-            String fileName = firstName + "_" + lastName + ".pdf";
-
-            amazonS3.putObject(new PutObjectRequest(awsClientConfig.getBucketName(), fileName, localFile));
-
             userService.createUser(user);
+            String fileName = user.getFirstName() + "_" + user.getLastName() + ".pdf";
+
+            PutObjectRequest putObjectRequest = new PutObjectRequest(awsClientConfig.getBucketName(),fileName, cv.getInputStream(), null);
+            amazonS3.putObject(putObjectRequest);
+
 
             return "User created successfully with CV uploaded!";
         } catch (IOException e) {
             e.printStackTrace();
             return "Error creating user: " + e.getMessage();
         }
-    }
-
-    private File convertMultipartFileToFile(MultipartFile file) throws IOException {
-        File convertedFile = new File(file.getOriginalFilename());
-        file.transferTo(convertedFile);
-        return convertedFile;
     }
 }
--- src/main/java/cloudcomputing/cc/entity/User.java
+++ src/main/java/cloudcomputing/cc/entity/User.java
@@ -1,9 +1,16 @@
 package cloudcomputing.cc.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
 
 @Entity
 @Table(name="users")
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,44 +24,4 @@ public class User {
     @Column(nullable = false)
     private String phone;
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
 }
