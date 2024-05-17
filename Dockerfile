01ce004a752f600c2b7d3ebac393bf4f14ceb713
DanaMihaela97 <danamaritca@gmail.com>
2024-05-17 19:51:05 +0300

    s3

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
-
 FROM maven:3.9.3-eclipse-temurin-17 AS build
 WORKDIR /app
 COPY pom.xml .
@@ -6,10 +5,7 @@ COPY src ./src
 RUN mvn clean package -DskipTests
 
 FROM eclipse-temurin:17-alpine
-COPY credentials ~/.aws/credentials
-COPY config ~/.aws/config
 WORKDIR /app
 COPY --from=build /app/target/*.jar /app/app.jar
-
 EXPOSE 7200
 ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/cloudcomputing/cc/controller/UserController.java
+++ src/main/java/cloudcomputing/cc/controller/UserController.java
@@ -4,15 +4,12 @@ import cloudcomputing.cc.entity.User;
 import cloudcomputing.cc.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.GetObjectResponse;
-
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.core.sync.RequestBody;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/api/v1/openingjobs")
@@ -25,24 +22,16 @@ public class UserController {
         this.userService = userService;
         this.s3Client = s3Client;
     }
-
     @PostMapping(path = "/apply", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public String createUser(@RequestPart User user, @RequestPart("cv") MultipartFile cv) throws IOException {
 
 //        String cvFileName = cv.getOriginalFilename();
-//        userService.createUser(user);
-
-        ResponseInputStream<GetObjectResponse> response = s3Client.getObject(
-                request -> request.bucket("cvs-ccproject").key("file-name.txt"));
-
-        return StreamUtils.copyToString(response, StandardCharsets.UTF_8);
+        userService.createUser(user);
+        PutObjectRequest objReq = PutObjectRequest.builder()
+                .bucket("cvs-ccproject")
+                .key(user.getFirstName() + user.getLastName())
+                .build();
+        s3Client.putObject(objReq, RequestBody.fromInputStream(cv.getInputStream(), cv.getInputStream().available()));
+        return "ok";
     }
-    @GetMapping("/test")
-    public String testS3() throws IOException{
-        ResponseInputStream<GetObjectResponse> response = s3Client.getObject(
-                request -> request.bucket("cvs-ccproject").key("file-name.txt"));
-
-        return StreamUtils.copyToString(response, StandardCharsets.UTF_8);
-    }
-
 }
