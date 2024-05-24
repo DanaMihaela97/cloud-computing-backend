91daad9216b34fc9a72d9980ce4649e2ea6db9e6
DanaMihaela97 <danamaritca@gmail.com>
2024-05-24 14:18:35 +0300

    job added notif

---
+++ src/main/java/cloudcomputing/cc/config/SnsPublisher.java
@@ -0,0 +1,28 @@
+package cloudcomputing.cc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sns.model.PublishRequest;
+@Configuration
+public class SnsPublisher {
+    @Bean
+    public SnsPublisher snsPublisher(SnsClient snsClient) {
+        return new SnsPublisher(snsClient);
+    }
+
+    private final SnsClient snsClient;
+
+    public SnsPublisher(SnsClient snsClient) {
+        this.snsClient = snsClient;
+    }
+
+    public void sendEmail(String subject, String bodyText) {
+        PublishRequest request = PublishRequest.builder()
+                .topicArn("arn:aws:sns:us-east-1:814615723430:cc-sns")
+                .subject(subject)
+                .message(bodyText)
+                .build();
+        snsClient.publish(request);
+    }
+}
